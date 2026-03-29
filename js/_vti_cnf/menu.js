3ec7d2a92edcca50c95ca33144a5f21a7e2403c9
prom64 <robstorme@gmail.com>
2026-03-29 10:12:34 +0200

    Add files via upload

---
+++ js/_vti_cnf/menu.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ROBS-DESKTOP\\robs2
+vti_modifiedby:SR|ROBS-DESKTOP\\robs2
+vti_timelastmodified:TR|22 Mar 2026 09:05:18 -0000
+vti_timecreated:TR|22 Mar 2026 09:05:18 -0000
+vti_cacheddtm:TX|22 Mar 2026 09:05:18 -0000
+vti_filesize:IR|3616
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|template-info.html dwt/site.dwt herinneringen.html dwt/site_f.dwt about.html memoriam_f.html site-map.html privacy-policy.html 2024_fr.htm memoriam.html reunies.html home_f.html index.html
